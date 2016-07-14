27b85bf2133573d47eab1ebdcd88f0eb2a317448
Josh T <jptucker2009@gmail.com>
2016-07-14 16:31:42 -0400

    JPA added with model package

---
+++ src/model/Spuser.java
@@ -0,0 +1,100 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.List;
+
+
+/**
+ * The persistent class for the SPUSERS database table.
+ * 
+ */
+@Entity
+@Table(name="SPUSERS")
+@NamedQuery(name="Spuser.findAll", query="SELECT s FROM Spuser s")
+public class Spuser implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@SequenceGenerator(name="SPUSERS_USERID_GENERATOR", sequenceName="SPUSERS_SEQ")
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="SPUSERS_USERID_GENERATOR")
+	private long userid;
+
+	private String hashedpassword;
+
+	private String salt;
+
+	private String useremail;
+
+	private String username;
+
+	//bi-directional many-to-one association to Sporder
+	@OneToMany(mappedBy="spuser")
+	private List<Sporder> sporders;
+
+	public Spuser() {
+	}
+
+	public long getUserid() {
+		return this.userid;
+	}
+
+	public void setUserid(long userid) {
+		this.userid = userid;
+	}
+
+	public String getHashedpassword() {
+		return this.hashedpassword;
+	}
+
+	public void setHashedpassword(String hashedpassword) {
+		this.hashedpassword = hashedpassword;
+	}
+
+	public String getSalt() {
+		return this.salt;
+	}
+
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
+
+	public String getUseremail() {
+		return this.useremail;
+	}
+
+	public void setUseremail(String useremail) {
+		this.useremail = useremail;
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public List<Sporder> getSporders() {
+		return this.sporders;
+	}
+
+	public void setSporders(List<Sporder> sporders) {
+		this.sporders = sporders;
+	}
+
+	public Sporder addSporder(Sporder sporder) {
+		getSporders().add(sporder);
+		sporder.setSpuser(this);
+
+		return sporder;
+	}
+
+	public Sporder removeSporder(Sporder sporder) {
+		getSporders().remove(sporder);
+		sporder.setSpuser(null);
+
+		return sporder;
+	}
+
+}
