00e45ae5fc57716f5a60d838dfc5a6eb32ca0427
brooney27 <brooney278@gmail.com>
2016-07-15 15:33:54 -0400

    B - confirm order done

--- src/orderControllers/ConfirmOrder.java
+++ src/orderControllers/ConfirmOrder.java
@@ -1,14 +1,18 @@
 package orderControllers;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import Functions.DBFunctions;
 import model.Sporder;
+import model.Spuser;
 
 /**
  * Servlet implementation class ConfirmOrder
@@ -29,13 +33,17 @@ public class ConfirmOrder extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Sporder order = DBFunctions.getOrderByID(request.getParameter("orderid"));
+		HttpSession session = request.getSession();
+		Spuser user = (Spuser)session.getAttribute("user");
 		
-		order.setStatus("1");
+		List<Sporder> orders = DBFunctions.getOrders(user.getUserid(), 2);
 		
-		DBFunctions.update(order);
+		for(Sporder o:orders){
+			o.setStatus("1");
+			DBFunctions.update(o);
+		}
 		
-		request.getRequestDispatcher("/Cart");
+		request.getRequestDispatcher("/Profile").forward(request, response);
 	}
 
 	/**
