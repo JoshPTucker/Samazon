ce269c365c95aec28b316594cdae47264544a51e
brooney27 <brooney278@gmail.com>
2016-07-18 10:59:02 -0400

    B-unit tests

---
+++ unitTests/tests/DBFunctionTests.java
@@ -0,0 +1,34 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import Functions.DBFunctions;
+import model.Sporder;
+import model.Spproduct;
+
+public class DBFunctionTests {
+
+	@Test
+	public void getOrdersTest() {
+		
+		List<Sporder> o = DBFunctions.getOrders(1,2);
+		
+		assertTrue(o.size()==3);
+		
+	}
+	
+	@Test
+	public void getUserTest() {
+		
+		Spproduct p = DBFunctions.getProductByID("10");
+		
+		assertTrue(p.getProductid()==10);
+		
+	}
+	
+	
+}
