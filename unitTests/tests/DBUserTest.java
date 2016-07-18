ec5b79c39086452b3bb3934af979801300e6c9d1
Josh T <jptucker2009@gmail.com>
2016-07-18 10:52:46 -0400

    josh unit test added

--- unitTests/tests/DBUserTest.java
+++ unitTests/tests/DBUserTest.java
@@ -4,11 +4,21 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import model.Spuser;
+import Functions.DBUser;
 public class DBUserTest {
 
+	
+	@Test
+	public void test_getvaliduser(){
+		String email= "u1@d.com";
+		String password="password";
+		Spuser  user = DBUser.getValidUser(email, password);
+		assertTrue(user!=null);
+	}
 	@Test
 	public void test() {
 		fail("Not yet implemented");
 	}
-
+	
 }
